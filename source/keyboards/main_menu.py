f2006a9e5c685855122a93fccbf0dd290d0c87f2
David-Roklem <roklem@mail.ru>
2025-01-25 10:23:48 +0300

    feature_implement_main_menu

---
+++ source/keyboards/main_menu.py
@@ -0,0 +1,13 @@
+from aiogram import Bot
+from aiogram.types import BotCommand
+
+from texts.bot_texts import TEXTS_CMD
+
+
+# Функция для настройки кнопки Menu бота
+async def set_main_menu(bot: Bot):
+    main_menu_commands = [
+        BotCommand(command=command, description=description)
+        for command, description in TEXTS_CMD.items()
+    ]
+    await bot.set_my_commands(main_menu_commands)
