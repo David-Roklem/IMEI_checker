c85d0aa1b0a0f50fa9f8d267e4355ea508e54c7a
David-Roklem <roklem@mail.ru>
2025-01-25 10:13:18 +0300

    feature_add_base_handlers

---
+++ source/handlers/base_handlers.py
@@ -0,0 +1,18 @@
+from aiogram import Router
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
+from texts.bot_texts import TEXTS
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def process_start_command(message: Message):
+    """Обработка команды '/start'"""
+    await message.answer(TEXTS[message.text])
+
+
+@router.message(Command(commands='help'))
+async def process_help_command(message: Message):
+    """Обработка команды '/help'"""
+    await message.answer(TEXTS[message.text])
