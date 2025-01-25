5ae8fe44b88d5ab953a32855691330cb7853f42e
David-Roklem <roklem@mail.ru>
2025-01-25 10:25:13 +0300

    feature_implement_entrypoint

---
+++ source/main.py
@@ -0,0 +1,38 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from config_data.config import config
+from handlers import base_handlers
+from keyboards.main_menu import set_main_menu
+
+
+logger = logging.getLogger(__name__)
+
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(filename)s:%(lineno)d #%(levelname)-8s "
+               "[%(asctime)s] - %(name)s - %(message)s")
+
+    logger.info("Starting bot")
+
+    bot = Bot(
+        token=config.bot_token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+    dp = Dispatcher()
+
+    await set_main_menu(bot)
+
+    dp.include_router(base_handlers.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
