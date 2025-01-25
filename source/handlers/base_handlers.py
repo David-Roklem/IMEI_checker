a469b058aad846953b0d7f96ef8ee4e9e04d40b2
David-Roklem <roklem@mail.ru>
2025-01-25 15:23:47 +0300

    feature_add_keyboard_for_base_handlers

--- source/handlers/base_handlers.py
+++ source/handlers/base_handlers.py
@@ -2,6 +2,7 @@ from aiogram import Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 from texts.bot_texts import TEXTS_CMD
+from keyboards.reply_keyboard import keyboard
 
 router = Router()
 
@@ -9,10 +10,10 @@ router = Router()
 @router.message(CommandStart())
 async def process_start_command(message: Message):
     """Обработка команды '/start'"""
-    await message.answer(TEXTS_CMD[message.text])
+    await message.answer(TEXTS_CMD[message.text], reply_markup=keyboard)
 
 
 @router.message(Command(commands='help'))
 async def process_help_command(message: Message):
     """Обработка команды '/help'"""
-    await message.answer(TEXTS_CMD[message.text])
+    await message.answer(TEXTS_CMD[message.text], reply_markup=keyboard)
