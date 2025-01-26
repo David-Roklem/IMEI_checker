457b453998a16b899c46daf7689adc8ba2188c2f
David-Roklem <roklem@mail.ru>
2025-01-26 10:07:04 +0300

    refact_make_reply_kb_one_time_keyboard

--- source/keyboards/reply_keyboard.py
+++ source/keyboards/reply_keyboard.py
@@ -5,4 +5,4 @@ button_test_mode = KeyboardButton(text='🧪 На тестовых данных'
 button_prod_mode = KeyboardButton(text='🛠 На реальных данных')
 
 keyboard = ReplyKeyboardMarkup(keyboard=[[button_test_mode, button_prod_mode]],
-                               resize_keyboard=True)
+                               resize_keyboard=True, one_time_keyboard=True)
