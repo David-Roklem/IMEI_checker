da13c7e233434666716cce59acf21d67afa35aaa
David-Roklem <roklem@mail.ru>
2025-01-26 09:29:14 +0300

    fix_clear_states_after_calling_base_cmds

--- source/handlers/base_handlers.py
+++ source/handlers/base_handlers.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.fsm.context import FSMContext
 from texts.bot_texts import TEXTS_CMD
 from keyboards.reply_keyboard import keyboard
 
@@ -8,12 +9,14 @@ router = Router()
 
 
 @router.message(CommandStart())
-async def process_start_command(message: Message):
+async def process_start_command(message: Message, state: FSMContext):
     """Обработка команды '/start'"""
     await message.answer(TEXTS_CMD[message.text], reply_markup=keyboard)
+    await state.clear()
 
 
 @router.message(Command(commands='help'))
-async def process_help_command(message: Message):
+async def process_help_command(message: Message, state: FSMContext):
     """Обработка команды '/help'"""
     await message.answer(TEXTS_CMD[message.text], reply_markup=ReplyKeyboardRemove())
+    await state.clear()
